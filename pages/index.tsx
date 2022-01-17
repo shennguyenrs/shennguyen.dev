ced5a092625e242b5d6a0336cd9dc09666cd5813
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2022-01-18 01:16:24 +0200

    feat: add about section and styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,13 @@ const Home: NextPage = () => {
         <div className={styles.navigation}>
           <ul className={styles.navigation__list}>
             <li className={styles.navigation__link}>
-              <a href="#porfolioSection">Porfolio</a>
+              <a href="#porfolio">Porfolio</a>
             </li>
             <li className={styles.navigation__link}>
-              <a href="#aboutSection">About</a>
+              <a href="#about">About</a>
             </li>
             <li className={styles.navigation__link}>
-              <a href="#contactSection">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
           </ul>
         </div>
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
             className={styles.heroFooter__p}
             onMouseEnter={() => setOnHover(true)}
             onMouseLeave={() => setOnHover(false)}
-            href="#porfolioSection"
+            href="#porfolio"
           >
             See my works
           </a>
@@ -54,6 +54,32 @@ const Home: NextPage = () => {
           >
             <img src="/images/arrow_tiny.png" alt="arrow" />
           </div>
+        </div>
+      </div>
+      <div className={styles.about}>
+        <p className={styles.sectionHeader}>About me</p>
+        <div className={styles.about__container}>
+          <div className={styles.about__img}>
+            <img src="/images/me_tiny.png" alt="myself-img" />
+          </div>
+          <div className={styles.about__p}>
+            <p>
+              I spend most of my time discovering, practising, making mistakes
+              and learning. I like the challenges, which will broaden my limits.
+              My goal is to learn to be a full-stack developer who wants to
+              bring creativity and technology into the success of businesses.
+              <br /> <br />
+              My works are powered Figma, Neovim and Google.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <p className={styles.footer__p}>Handcrafted by me. Made in 🇫🇮</p>
+        <div className={styles.footer__social}>
+          <img src="/images/linkedin_tiny.png" alt="linkedin" />
+          <img src="/images/at_tiny.png" alt="at" />
+          <img src="/images/whatsapp_tiny.png" alt="whatsapp" />
         </div>
       </div>
     </>
