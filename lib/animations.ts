ead1070610d7f5bdf56032a89fb30e896a8e4b7e
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2022-02-14 15:16:47 +0200

    styles: remove scale on transition

--- lib/animations.ts
+++ lib/animations.ts
@@ -4,21 +4,18 @@ export const slideRight = {
     initial: {
       opacity: 0,
       left: '-100%',
-      scale: 0.6,
     },
     animate: {
       opacity: 1,
       left: 0,
-      scale: 1,
     },
     exit: {
       opacity: 0,
       left: '100%',
-      scale: 0.6,
     },
   },
   transition: {
-    duration: 0.7,
+    duration: 0.8,
   },
 };
 
