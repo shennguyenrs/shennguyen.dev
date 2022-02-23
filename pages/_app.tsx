6862137a47d122b57b5bd677be80e6be02b06af0
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2022-02-24 00:25:10 +0200

    styles: change font request to all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,7 +44,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&text=nPeArg,osajbylMtumNHicf404OTFUD&family=Rubik:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Rubik:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
           rel="stylesheet"
         />
         <link rel="manifest" href="/site.webmanifest" />
